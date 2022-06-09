d314db327d94364557cd04b98bd2e4b71e025ba5
Anel <abraranel@gmail.com>
2022-06-10 00:07:24 +0600

    add solution for task 6

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-//1
+//5
 function randomInteger(min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
 }
 alert(randomInteger(1, 5));
 alert(randomInteger(1, 5));
 alert(randomInteger(1, 5));
+
+//6
+function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+    return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+}
+console.log(checkSpam('buy ViAgRA now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam("innocent rabbit"));
