8b3192fa7293839be86c0902369423a091aaff13
Anel <abraranel@gmail.com>
2022-06-09 23:55:32 +0600

    add solution for task 5

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 //1
-//let i = 3;
-//while (i) {
-//  alert( i-- );
-//} // последним значением будет 1, тк i-- будет уменьшеть значение пока i=0, т.е. сначла будет выведено 3, затем уменьшиться на один до 2 и тд
-
-//2
-let i = 0;
-while (i < 3) {
-    alert(`number ${i}!`);
-    i++
+function randomInteger(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
 }
-
-//3
-let number;
-do {
-    number = +prompt('Введите число больше 100', 0);
-} while (number <= 100 || null);
-
-//4
-let num = 0;
-let n = 1000;
-while (n >= 50) {
-    n /= 2;
-    num++;
-    alert(num);
-}
-//for (let n = 1000; n < 50; num++) {
-//    n /= 2;
-//}
-//alert(num);
+alert(randomInteger(1, 5));
+alert(randomInteger(1, 5));
+alert(randomInteger(1, 5));
